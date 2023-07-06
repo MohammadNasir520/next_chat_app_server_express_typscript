d1b63f383b970257ddb1ccf8a1224c3b44032635
MohammadNasir520 <nasirahsan520@gmail.com>
2023-07-06 20:22:54 +0600

    correction

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,6 +2,6 @@ import express from 'express';
 import { UserController } from './user.controller';
 const router = express.Router();
 
-router.post('/create-user', UserController.signup);
+router.post('/create-user', UserControllesdfr.signup);
 
 export const UserRoutes = router;
